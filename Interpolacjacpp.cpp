7ddd4e60f07bafd08f902d5ca3e6d492aaa2c524
Curriosityy <patryk9595@gmail.com>
2017-03-21 15:30:48 +0100

    Interpolacja newtona z różnicami progresywnymi

--- Interpolacjacpp.cpp
+++ Interpolacjacpp.cpp
@@ -4,8 +4,48 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
-
-
+int silnia(int i)
+{
+	int sil=1;
+	for (int j = 1; j <= i; j++)
+	{
+			sil *= j;
+	}
+	return sil;
+}
+void rozniceProgrwsywne(int tabx[], int taby[], double *rozProgTab[], int ilosc_w)
+{
+	for (int i = 0; i < ilosc_w; i++)
+	{
+		for (int j = 0; j < ilosc_w-i; j++)
+		{
+			if (i == 0)
+			{
+				rozProgTab[0][j] = taby[j];
+			}
+			else
+			{
+				rozProgTab[i][j] = rozProgTab[i-1][j + 1] - rozProgTab[i-1][j];
+			}
+		}
+	}
+}
+double interpolacjaNewtona2(int tabx[], int taby[], double *rozProgTab[], int ilosc_w,double x_szukany)
+{
+	double wx = rozProgTab[0][0];
+	double h = tabx[1] - tabx[0];
+	for (int i = 1; i < ilosc_w; i++)
+	{
+		double wj = 1;
+		for (int j = 0; j <= i-1; j++)
+		{
+			wj *= (x_szukany - tabx[j]);
+		}
+		double pod = (silnia(i)*pow(h, i));
+		wx += (wj*(rozProgTab[i][0]/pod));
+	}
+	return wx;
+}
 void ilorazyRoznicowe(int tabx[], int taby[], double *tabiloczyny[], int ilosc_w)
 {
 	for (int i = 0; i < ilosc_w; i++)
@@ -23,6 +63,8 @@ void ilorazyRoznicowe(int tabx[], int taby[], double *tabiloczyny[], int ilosc_w
 		}
 	}
 }
+
+
 double interpolacjaNewtona1(int tabx[], int taby[], double *tabiloczyny[], int ilosc_w,double x_szukany)
 {
 	double wx = 0;
@@ -71,6 +113,11 @@ int main()
 	{
 		tabiloczyny[i] = new double[ilosc_w];
 	}
+	double **rozProgTab = new double*[ilosc_w];
+	for (int i = 0; i < ilosc_w; i++)
+	{
+		rozProgTab[i] = new double[ilosc_w];
+	}
 	for (int i = 0; i < ilosc_w; i++)
 	{
 		cin >> tabx[i];
@@ -81,7 +128,8 @@ int main()
 	cout << "Lagrange'a dla x " << x_szukany << " y to " << interpolacjaLagrangea(ilosc_w, tabx, taby, x_szukany) << endl;
 	ilorazyRoznicowe(tabx,taby,tabiloczyny,ilosc_w);
 	cout << "Newtona ilorazy dla x " << x_szukany << " y to " << interpolacjaNewtona1(tabx, taby, tabiloczyny, ilosc_w, x_szukany) << endl;
-	
+	rozniceProgrwsywne(tabx, taby, rozProgTab, ilosc_w);
+	cout << "Newtona progrestwna dla x " << x_szukany << " y to " << interpolacjaNewtona2(tabx, taby, rozProgTab, ilosc_w, x_szukany) << endl;
 	getch();
 	return EXIT_SUCCESS;
 }
